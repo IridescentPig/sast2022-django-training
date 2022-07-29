a7c85c4dc393887f57ad2059b2da3679c062ea27
IridescentPig <IridescentPig@outlook.com>
2022-07-29 11:00:35 +0800

    Ver 1.0

--- lb/utils.py
+++ lb/utils.py
@@ -1,3 +1,5 @@
+from enum import EnumMeta
+from unicodedata import category
 from lb.models import Submission, User
 from random import randint
 import functools
@@ -59,7 +61,7 @@ def get_leaderboard():
         {
             "user": obj.user.username,
             "score": obj.score,
-            "subs": [int(x) for x in obj.subs.split()],
+            "subs": [float(x) for x in obj.subs.split(',')],
             "avatar": obj.avatar,
             "time": obj.time,
             "votes": obj.user.votes
@@ -70,17 +72,60 @@ def get_leaderboard():
     # 方案3：调用 Django 的 API (俺不会了
     # ...
 
+import math
+
+def interpolate(x1, x2, y1, y2, x):
+    if x < x1:
+        return y1
+    if x > x2:
+        return y2
+    return math.sqrt((x - x1) / (x2 - x1)) * (y2 - y1) + y1
+
+def main_score(result: list):
+    """
+    :param result: catagory accuracy, element value in [0, 1]
+    :return: main_score
+    """
+    mean_result = sum(result) / 3
+    return round(
+        55 * interpolate(.5, .8, 0, 1, mean_result) +
+        15 * interpolate(.5, .7, 0, 1, result[0]) +
+        15 * interpolate(.5, .9, 0, 1, result[1]) +
+        15 * interpolate(.5, .75, 0, 1, result[2])
+    )
+
 def judge(content: str):
     """
     Convert submitted content to a main score and a list of sub scors
     :param content: the submitted content to be judged
     :return: main score, list[sub score]
     """
-
     # TODO: Use `ground_truth.txt` and the content to calculate scores.
     #  If `content` is invalid, raise an Exception so that it can be
     #  captured in the view function.
     #  You can define the calculation of main score arbitrarily.
-
-    subs = [randint(0, 100) for _ in range(3)]
-    return sum(subs), subs
+    with open('lb/ground_truth.txt', 'r') as f:
+        ground_truth_raw = f.read()
+    ground_truth = ground_truth_raw.split('\n')[1:]
+    ground_truth.pop()
+    subs = [0, 0, 0]
+    content_cols = content.split('\n')
+    if content_cols[-1] == '':
+        content_cols.pop()
+    if len(content_cols) != 1000:
+        raise Exception
+    for index, col in enumerate(content_cols):
+        col_result = col.split(',')
+        if len(col_result) != 3:
+            raise Exception
+        col_ground_truth = ground_truth[index].split(',')[1:]
+        for i, category_flag in enumerate(col_result):
+            if int(category_flag) not in (0, 1):
+                raise Exception
+            if int(category_flag) == eval(col_ground_truth[i]):
+                subs[i] += 1
+    for i, sub in enumerate(subs):
+        subs[i] = sub / 1000
+    
+    #subs = [randint(0, 100) for _ in range(3)]
+    return main_score(subs), subs
--- lb/views.py
+++ lb/views.py
@@ -1,8 +1,11 @@
+from email.policy import strict
+from re import sub
 from django.http import (
     HttpRequest,
     JsonResponse,
     HttpResponseNotAllowed,
 )
+from pip import main
 from lb.models import Submission, User
 from django.forms.models import model_to_dict
 from django.db.models import F
@@ -11,6 +14,7 @@ from lb import utils
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.views.decorators.http import require_http_methods as method
+import time
 
 def hello(req: HttpRequest):
     return JsonResponse({
@@ -19,6 +23,7 @@ def hello(req: HttpRequest):
     })
 
 # TODO: Add HTTP method check
+@method(["GET"])
 def leaderboard(req: HttpRequest):
     return JsonResponse(
         utils.get_leaderboard(),
@@ -27,18 +32,73 @@ def leaderboard(req: HttpRequest):
 
 
 @method(["GET"])
-def history(req: HttpRequest, username: int):
+def history(req: HttpRequest, username: str):
     # TODO: Complete `/history/<slug:username>` API
-
-    raise NotImplementedError
+    res = Submission.objects.filter(user__username = username).order_by('time')
+    if res.first():
+        return JsonResponse([
+            {
+                **model_to_dict(submisson, exclude = ['id', 'user', 'avatar'])
+            }
+            for submisson in res
+        ])
+    else:
+        return JsonResponse({
+            'code': -1
+        }, status = 404)
+    #raise NotImplementedError
 
 
 @method(["POST"])
 @csrf_exempt
 def submit(req: HttpRequest):
     # TODO: Complete `/submit` API
-
-    raise NotImplementedError
+    try:
+        req_json = json.loads(req.body, strict = False)
+        if 'user' in req_json and 'avatar' in req_json and 'content' in req_json:
+            if len(req_json['user']) > 255:
+                return JsonResponse({
+                    'code': -1,
+                    'msg': '用户名太长了'
+                })
+            if len(req_json['avatar']) > 1e5:
+                return JsonResponse({
+                    'code': -2,
+                    'msg': '图像太大了'
+                })
+            try:
+                main_score, subs = utils.judge(req_json['content'])
+            except:
+                return JsonResponse({
+                    'code': -3,
+                    'msg': '提交内容非法呜呜'
+                })
+            submit_user = User.objects.filter(username = req_json['user']).first()
+            if submit_user:
+                pass
+            else:
+                User.objects.create(username = req_json['user'])
+                submit_user = User.objects.filter(username = req_json['user']).first()
+            subs_str = ','.join([str(i) for i in subs])
+            Submission.objects.create(user = submit_user, avatar = req_json['avatar'], time = time.time(), score = main_score, subs = subs_str)
+            return JsonResponse({
+                'code': 0,
+                'msg': '提交成功',
+                'data': {
+                    'leaderboard': utils.get_leaderboard()
+                }
+            })
+        else:
+            return JsonResponse({
+                'code': 1,
+                'msg': '参数不全啊'
+            })
+    except:
+        return JsonResponse({
+            'code': -4,
+            'msg': '提交内容非法'
+        })
+    #raise NotImplementedError
 
 
 @method(["POST"])
@@ -49,7 +109,31 @@ def vote(req: HttpRequest):
         return JsonResponse({
             "code": -1
         })
-
+    try:
+        req_json = json.loads(req.body, strict = False)
+        if 'user' in req_json:
+            vote_user = User.objects.filter(username = req_json['user']).first()
+            if vote_user:
+                vote_user.votes = F('votes') + 1
+                vote_user.save()
+                return JsonResponse({
+                    'code': 0,
+                    'data': {
+                        'leaderboard': utils.get_leaderboard()
+                    }
+                })
+            else:
+                return JsonResponse({
+                    'code': -1,
+                })
+        else:
+            return JsonResponse({
+                'code': -1,
+            })
+    except:
+        return JsonResponse({
+            'code': -1,
+        })
     # TODO: Complete `/vote` API
 
     raise NotImplementedError
